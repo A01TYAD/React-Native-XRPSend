8f09773f4b4657b8f905700b90115c8cbd06bd59
Aditya Dhungana <adityadhungana@gmail.com>
2019-07-20 19:22:28 +0545

    Update TransactionHistory.js

--- src/TransactionHistory.js
+++ src/TransactionHistory.js
@@ -3,7 +3,6 @@ import React, {Component} from 'react';
 import {Platform, StyleSheet, Text, View, Button} from 'react-native';
 import AsyncStorage from '@react-native-community/async-storage';
 import { createStackNavigator, createAppContainer } from "react-navigation";
-import _ from 'lodash';
 const ripple = require('../ripple');
 
 export default class TransactionHistory extends React.Component {
